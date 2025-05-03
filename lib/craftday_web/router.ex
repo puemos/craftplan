b7a75d1a47b4a8e4600237e36d644d27638655d4
Shy Alter <puemos@gmail.com>
2025-05-03 08:51:07 +0200

    chore: routed reorg

--- lib/craftday_web/router.ex
+++ lib/craftday_web/router.ex
@@ -5,17 +5,9 @@ defmodule CraftdayWeb.Router do
   alias AshAuthentication.Phoenix.Overrides.Default
   alias Craftday.Cart
 
-  pipeline :browser do
-    plug :accepts, ["html"]
-    plug :fetch_session
-    plug :fetch_live_flash
-    plug :put_root_layout, html: {CraftdayWeb.Layouts, :root}
-    plug :protect_from_forgery
-    plug :put_secure_browser_headers
-    plug :load_from_session
-    plug :put_session_timezone
-    plug :put_cart
-  end
+  #
+  # Plugs
+  #
 
   def put_cart(conn, _opts) do
     cart =
@@ -31,21 +23,66 @@ defmodule CraftdayWeb.Router do
     put_session(conn, :cart_id, cart.id)
   end
 
-  pipeline :api do
-    plug :accepts, ["json"]
-    plug :load_from_bearer
-  end
-
   def put_session_timezone(conn, _opts) do
     timezone = conn.cookies["timezone"]
     put_session(conn, "timezone", timezone)
   end
 
+  #
+  # Pipelines
+  #
+
+  pipeline :browser do
+    plug :accepts, ["html"]
+    plug :fetch_session
+    plug :fetch_live_flash
+    plug :put_root_layout, html: {CraftdayWeb.Layouts, :root}
+    plug :protect_from_forgery
+    plug :put_secure_browser_headers
+    plug :load_from_session
+    plug :put_session_timezone
+    plug :put_cart
+  end
+
+  pipeline :api do
+    plug :accepts, ["json"]
+    plug :load_from_bearer
+  end
+
+  #
+  # Public Routes
+  #
+
   scope "/", CraftdayWeb do
     pipe_through :browser
 
+    get "/", PageController, :home
     post "/cart/clear", CartController, :clear
 
+    # Authentication Routes
+    auth_routes AuthController, Craftday.Accounts.User, path: "/auth"
+    sign_out_route AuthController
+
+    sign_in_route register_path: "/register",
+                  reset_path: "/reset",
+                  auth_routes_prefix: "/auth",
+                  on_mount: [
+                    CraftdayWeb.LiveCurrentPath,
+                    CraftdayWeb.LiveCart,
+                    {CraftdayWeb.LiveUserAuth, :live_no_user}
+                  ],
+                  overrides: [
+                    CraftdayWeb.AuthOverrides,
+                    Default
+                  ]
+
+    reset_route auth_routes_prefix: "/auth",
+                overrides: [
+                  CraftdayWeb.AuthOverrides,
+                  Default
+                ]
+
+    # Public LiveView Routes
     live_session :public,
       on_mount: [
         CraftdayWeb.LiveCurrentPath,
@@ -57,7 +94,16 @@ defmodule CraftdayWeb.Router do
       live "/cart", Public.CartLive.Index, :index
       live "/checkout", Public.CheckoutLive.Index, :index
     end
+  end
 
+  #
+  # Authenticated Routes
+  #
+
+  scope "/", CraftdayWeb do
+    pipe_through :browser
+
+    # Admin Routes
     ash_authentication_live_session :admin_routes,
       on_mount: [
         CraftdayWeb.LiveCurrentPath,
@@ -65,12 +111,14 @@ defmodule CraftdayWeb.Router do
         CraftdayWeb.LiveCart,
         {CraftdayWeb.LiveUserAuth, :live_admin_required}
       ] do
+      # Settings Routes
       live "/manage/settings", SettingsLive.Index, :index
       live "/manage/settings/general", SettingsLive.Index, :general
       live "/manage/settings/allergens", SettingsLive.Index, :allergens
       live "/manage/settings/nutritional_facts", SettingsLive.Index, :nutritional_facts
     end
 
+    # Staff Routes
     ash_authentication_live_session :manage_routes,
       on_mount: [
         CraftdayWeb.LiveCurrentPath,
@@ -78,6 +126,7 @@ defmodule CraftdayWeb.Router do
         CraftdayWeb.LiveCart,
         {CraftdayWeb.LiveUserAuth, :live_staff_required}
       ] do
+      # Products
       live "/manage/products", ProductLive.Index, :index
       live "/manage/products/new", ProductLive.Index, :new
       live "/manage/products/:sku", ProductLive.Show, :show
@@ -87,6 +136,7 @@ defmodule CraftdayWeb.Router do
       live "/manage/products/:sku/photos", ProductLive.Show, :photos
       live "/manage/products/:sku/edit", ProductLive.Show, :edit
 
+      # Inventory
       live "/manage/inventory", InventoryLive.Index, :index
       live "/manage/inventory/forecast", InventoryLive.Index, :forecast
       live "/manage/inventory/new", InventoryLive.Index, :new
@@ -98,6 +148,7 @@ defmodule CraftdayWeb.Router do
       live "/manage/inventory/:sku/edit", InventoryLive.Show, :edit
       live "/manage/inventory/:sku/adjust", InventoryLive.Show, :adjust
 
+      # Orders
       live "/manage/orders", OrderLive.Index, :index
       live "/manage/orders/new", OrderLive.Index, :new
       live "/manage/orders/:reference", OrderLive.Show, :show
@@ -105,6 +156,7 @@ defmodule CraftdayWeb.Router do
       live "/manage/orders/:reference/items", OrderLive.Show, :items
       live "/manage/orders/:reference/edit", OrderLive.Show, :edit
 
+      # Customers
       live "/manage/customers", CustomerLive.Index, :index
       live "/manage/customers/new", CustomerLive.Index, :new
       live "/manage/customers/:reference", CustomerLive.Show, :show
@@ -113,6 +165,7 @@ defmodule CraftdayWeb.Router do
       live "/manage/customers/:reference/statistics", CustomerLive.Show, :statistics
       live "/manage/customers/:reference/edit", CustomerLive.Index, :edit
 
+      # Planning
       live "/manage/plan", PlanLive.Index, :index
       live "/manage/plan/schedule", PlanLive.Index, :schedule
       live "/manage/plan/materials", PlanLive.Index, :materials
@@ -130,39 +183,18 @@ defmodule CraftdayWeb.Router do
     end
   end
 
-  scope "/", CraftdayWeb do
-    pipe_through :browser
-
-    get "/", PageController, :home
-    auth_routes AuthController, Craftday.Accounts.User, path: "/auth"
-    sign_out_route AuthController
-
-    # Remove these if you'd like to use your own authentication views
-    sign_in_route register_path: "/register",
-                  reset_path: "/reset",
-                  auth_routes_prefix: "/auth",
-                  on_mount: [
-                    CraftdayWeb.LiveCurrentPath,
-                    CraftdayWeb.LiveCart,
-                    {CraftdayWeb.LiveUserAuth, :live_no_user}
-                  ],
-                  overrides: [
-                    CraftdayWeb.AuthOverrides,
-                    Default
-                  ]
-
-    # Remove this if you do not want to use the reset password feature
-    reset_route auth_routes_prefix: "/auth",
-                overrides: [
-                  CraftdayWeb.AuthOverrides,
-                  Default
-                ]
-  end
+  #
+  # API Routes
+  #
 
   # Other scopes may use custom stacks.
   # scope "/api", CraftdayWeb do
   #   pipe_through :api
   # end
+
+  #
+  # Development Routes
+  #
 
   # Enable LiveDashboard and Swoosh mailbox preview in development
   if Application.compile_env(:craftday, :dev_routes) do
