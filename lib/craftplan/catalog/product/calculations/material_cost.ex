d3060a7098884742a3e0edc565a33e9698088827
Shy Alter <puemos@gmail.com>
2025-11-08 15:51:27 +0100

    refactor(catalog): simplify material cost calc

--- lib/craftplan/catalog/product/calculations/material_cost.ex
+++ lib/craftplan/catalog/product/calculations/material_cost.ex
@@ -1,61 +1,40 @@
 defmodule Craftplan.Catalog.Product.Calculations.MaterialCost do
-  @moduledoc false
+  @moduledoc """
+  Calculates the material cost for a product from its active BOM rollup.
+  """
 
   use Ash.Resource.Calculation
 
   alias Ash.NotLoaded
-  alias Craftplan.Catalog
-  alias Craftplan.Catalog.BOM
-  alias Craftplan.Catalog.Services.BatchCostCalculator
   alias Decimal, as: D
 
   @impl true
-  def init(_opts), do: {:ok, []}
+  def load(_query, _opts, _context), do: [active_bom: [rollup: [:material_cost]]]
 
   @impl true
-  # Avoid preloading via Calcs; fetch what we need inside
-  def load(_query, _opts, _context), do: []
-
-  @impl true
-  def calculate(records, _opts, context) do
-    actor = context.actor
-    authorize? = context.authorize?
-    Enum.map(records, &material_cost(&1, actor, authorize?))
+  def calculate(records, _opts, _context) do
+    Enum.map(records, &material_cost/1)
   end
 
-  defp material_cost(%{active_bom: %NotLoaded{}, id: product_id}, actor, authorize?) do
-    fetch_and_compute(product_id, actor, authorize?)
-  end
+  defp material_cost(record) do
+    rollup =
+      case Map.get(record, :active_bom) do
+        %NotLoaded{} -> nil
+        nil -> nil
+        bom -> Map.get(bom, :rollup)
+      end
 
-  defp material_cost(%{active_bom: nil, id: product_id}, actor, authorize?) do
-    fetch_and_compute(product_id, actor, authorize?)
-  end
+    material_cost =
+      case rollup do
+        %NotLoaded{} -> nil
+        nil -> nil
+        rollup -> Map.get(rollup, :material_cost)
+      end
 
-  defp material_cost(%{active_bom: bom}, _actor, _authorize?) do
-    case Map.get(bom, :rollup) do
-      %NotLoaded{} -> compute_material_cost(bom)
-      nil -> compute_material_cost(bom)
-      rollup -> Map.get(rollup, :material_cost) || D.new(0)
+    case material_cost do
+      %NotLoaded{} -> D.new(0)
+      nil -> D.new(0)
+      cost -> cost
     end
   end
-
-  defp fetch_and_compute(product_id, actor, authorize?) do
-    case Catalog.get_active_bom_for_product(%{product_id: product_id},
-           actor: actor,
-           authorize?: authorize?
-         ) do
-      {:ok, %BOM{} = bom} -> compute_material_cost(bom)
-      _ -> D.new(0)
-    end
-  end
-
-  defp compute_material_cost(bom) do
-    bom
-    |> BatchCostCalculator.calculate(D.new(1), authorize?: false)
-    |> Map.get(:material_cost, D.new(0))
-  rescue
-    _ -> D.new(0)
-  end
-
-  # no recipe fallback
 end
