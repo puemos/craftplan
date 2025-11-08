f3edb4cb94f422649b531877a8f3d500eb9225e9
Shy Alter <puemos@gmail.com>
2025-11-08 15:50:17 +0100

    refactor(catalog): simplify unit cost calc

--- lib/craftplan/catalog/product/calculations/unit_cost.ex
+++ lib/craftplan/catalog/product/calculations/unit_cost.ex
@@ -1,61 +1,38 @@
 defmodule Craftplan.Catalog.Product.Calculations.UnitCost do
-  @moduledoc false
+  @moduledoc """
+  Calculates the total unit cost for a product from its active BOM rollup.
+  """
 
   use Ash.Resource.Calculation
 
   alias Ash.NotLoaded
-  alias Craftplan.Catalog
-  alias Craftplan.Catalog.BOM
-  alias Craftplan.Catalog.Services.BatchCostCalculator
   alias Decimal, as: D
 
   @impl true
-  def init(_opts), do: {:ok, []}
+  def load(_query, _opts, _context), do: [active_bom: [rollup: [:unit_cost]]]
 
   @impl true
-  # Do not rely on Ash preloading for this calculation; we'll fetch what we need.
-  def load(_query, _opts, _context), do: []
+  def calculate(records, _opts, _context) do
+    Enum.map(records, fn record ->
+      rollup =
+        case Map.get(record, :active_bom) do
+          %NotLoaded{} -> nil
+          nil -> nil
+          bom -> Map.get(bom, :rollup)
+        end
 
-  @impl true
-  def calculate(records, _opts, context) do
-    actor = context.actor
-    authorize? = context.authorize?
-    Enum.map(records, &unit_cost(&1, actor, authorize?))
+      unit_cost =
+        case rollup do
+          %NotLoaded{} -> nil
+          nil -> nil
+          rollup -> Map.get(rollup, :unit_cost)
+        end
+
+      case unit_cost do
+        %NotLoaded{} -> D.new(0)
+        nil -> D.new(0)
+        cost -> cost
+      end
+    end)
   end
-
-  defp unit_cost(%{active_bom: %NotLoaded{}, id: product_id}, actor, authorize?) do
-    fetch_and_compute(product_id, actor, authorize?)
-  end
-
-  defp unit_cost(%{active_bom: nil, id: product_id}, actor, authorize?) do
-    fetch_and_compute(product_id, actor, authorize?)
-  end
-
-  defp unit_cost(%{active_bom: bom}, _actor, _authorize?) do
-    case Map.get(bom, :rollup) do
-      %NotLoaded{} -> compute_unit_cost(bom)
-      nil -> compute_unit_cost(bom)
-      rollup -> Map.get(rollup, :unit_cost) || D.new(0)
-    end
-  end
-
-  defp fetch_and_compute(product_id, actor, authorize?) do
-    case Catalog.get_active_bom_for_product(%{product_id: product_id},
-           actor: actor,
-           authorize?: authorize?
-         ) do
-      {:ok, %BOM{} = bom} -> compute_unit_cost(bom)
-      _ -> D.new(0)
-    end
-  end
-
-  defp compute_unit_cost(bom) do
-    bom
-    |> BatchCostCalculator.calculate(D.new(1), authorize?: false)
-    |> Map.get(:unit_cost, D.new(0))
-  rescue
-    _ -> D.new(0)
-  end
-
-  # no recipe fallback
 end
