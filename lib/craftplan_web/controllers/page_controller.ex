d0cf10b35b7adb363503b60f76b382ea1ff7b27c
Shy Alter <puemos@gmail.com>
2026-01-30 20:17:42 +0100

    feat(auth): redirect logged-in users from home to production plan

--- lib/craftplan_web/controllers/page_controller.ex
+++ lib/craftplan_web/controllers/page_controller.ex
@@ -2,16 +2,20 @@ defmodule CraftplanWeb.PageController do
   use CraftplanWeb, :controller
 
   def home(conn, _params) do
-    release_version =
-      case Application.spec(:craftplan, :vsn) do
-        nil -> "dev"
-        version -> to_string(version)
-      end
+    if conn.assigns[:current_user] do
+      redirect(conn, to: ~p"/manage/production/plan")
+    else
+      release_version =
+        case Application.spec(:craftplan, :vsn) do
+          nil -> "dev"
+          version -> to_string(version)
+        end
 
-    conn
-    |> assign(:current_path, "/")
-    |> assign(:page_title, "Craftplan")
-    |> put_layout(false)
-    |> render(:home, release_version: release_version)
+      conn
+      |> assign(:current_path, "/")
+      |> assign(:page_title, "Craftplan")
+      |> put_layout(false)
+      |> render(:home, release_version: release_version)
+    end
   end
 end
